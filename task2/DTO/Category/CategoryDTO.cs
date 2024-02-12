99305edfd99d5b084b59e33168eb91793f0751e8
raniataweel <raniatwaeel88@gmail.com>
2024-02-12 15:12:35 +0300

    r

--- task2/DTO/Category/CategoryDTO.cs
+++ task2/DTO/Category/CategoryDTO.cs
@@ -1,8 +1,7 @@
 ﻿namespace task2.DTO.Category
 {
     public class CategoryDTO
-    {
-        public string Name { get; set; }
+    {   public string Name { get; set; }
         public string Description { get; set; }
         public int CategoryId { get; set; }
     }
